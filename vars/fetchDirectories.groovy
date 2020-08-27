86d8c286ce3f87267a32481518f65cbe9b4bb4d3
Bhadre <70216877+Bhadre@users.noreply.github.com>
2020-08-27 14:24:33 +0530

    Create fetchDirectories.groovy

---
+++ vars/fetchDirectories.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    stages {
+        stage('List Directory Stage') {
+          steps {
+            bat label: '', script: '''C:
+            cd /
+            dir'''
+            echo "Here are the directories in C Drive"
+          }
+        }
+        stage('Find Current Directory Stage') {
+          steps {
+            bat label: '', script: '''C:
+            cd /'''
+            echo "Current Directory is root C:"
+          }
+        }
+    }
+}
